28de809d62aaee85e05bc1a11e6420cb093d5e53
Tera Earlywine <dev@teraearlywine.com>
2024-07-24 15:17:40 -0700

    *

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,16 +1,15 @@
 import argparse
 import os
-import json
 from auto_code.auto_code import AutoCode
 
-TEMP_FILE = 'temp_files.json'
 
 def main():
     """Main entry point for the Auto Code CLI Tool."""
     parser = argparse.ArgumentParser(description="Auto Code CLI Tool")
     subparsers = parser.add_subparsers(dest='command', required=True)
 
-    parser_get_files = subparsers.add_parser('refine', help='Get list of files in directory with given extension, sends to refinery for updates')
+    parser_get_files = subparsers.add_parser('refine', 
+                            help='Get list of files in directory with given extension, sends to refinery for updates')
     parser_get_files.add_argument('dir', type=str, help='Directory containing files')
     parser_get_files.add_argument('ext', type=str, help='File extension to load')
 
