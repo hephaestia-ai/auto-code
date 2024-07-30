ec448b55e109471dca3085c784ec2a46ab61f9e9
Tera Earlywine <dev@teraearlywine.com>
2024-07-30 09:00:04 -0700

    *

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_requirements():
 
 setup(
     name="cowgirl-ai-auto-code",
-    version="1.3.5",
+    version="1.3.6",
     description="Cowgirl AI - Auto Code",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -24,11 +24,11 @@ setup(
     python_requires=">=3.10",
     entry_points={
         "console_scripts": [
-            "cowgirl-ai-auto-code=cli.main:main", 
+            "auto-code=cli.main:main", 
             "front_end=cli.front_end:main",   # front_end refine --file='test.js'
             "back_end=cli.back_end:main",     # back_end refine --file='test.py'
             "qa=cli.qa:main",                  # qa refine --file='test.py'
-            "script=cli.scripting_bot:main"    # script refine --file='test.sh'
+            "script=cli.scripting_bot:main",   # script refine --file='test.sh'
             "html_css=cli.html_css:main"       # html_css refine --file='test.html'
         ],
     },
